f031b1023e49ebd17b122df9bc72dfbd62db72a5
r11i <wodeshouji12@gmail.com>
2024-06-09 10:29:53 +0700

    navbar & notif

--- pages/screens/home.js
+++ pages/screens/home.js
@@ -5,18 +5,21 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser, faEnvelope, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 import dynamic from "next/dynamic";
 const inter = Inter({ subsets: ["latin"] });
+import Router, { useRouter } from "next/router";
 
 const Map = dynamic(() => import("./map"), {ssr: false})
 
 export default function Home() {
+  const router = useRouter();
+  const {userId} = router.query
   return (
     <main style={{position: 'relative', height: '100vh', width: '100vw'}}>
       <Map></Map>
       <div style={{padding: '20px', height: '100vh', position: 'absolute', left: 0, top: 0, backgroundColor: 'white', zIndex: 9999, borderTopRightRadius: 20, borderBottomRightRadius: 20 }}>
-        <Link style={{display: 'block'}} href={'/screens/home'}>Home</Link>
-        <Link style={{display: 'block', marginTop: 20}} href={'/screens/history'}>History</Link>
-        <Link style={{display: 'block', marginTop: 20}} href={'/screens/notification'}>Notification</Link>
-        <Link style={{display: 'block', marginTop: 20}} href={'/screens/profil'}>Profile</Link>
+        <Link style={{display: 'block'}} href={`/screens/home?userId=${userId}`}>Home</Link>
+        <Link style={{display: 'block', marginTop: 20}} href={`/screens/history?userId=${userId}`}>History</Link>
+        <Link style={{display: 'block', marginTop: 20}} href={`/screens/notification?userId=${userId}`}>Notification</Link>
+        <Link style={{display: 'block', marginTop: 20}} href={`/screens/profil?userId=${userId}`}>Profile</Link>
       </div>
     </main>
   );
--- pages/screens/notification.js
+++ pages/screens/notification.js
@@ -1,64 +1,77 @@
 // components/NotificationComponent.js
 import React, { useEffect, useState } from 'react';
-import styles from './NotificationComponent.module.css';
-
+import { firestore, firebaseAuth } from '../../config/firebase';
+import { collection, addDoc, query, where, getDocs, updateDoc, Timestamp, getDoc } from 'firebase/firestore';
+import Router, { useRouter } from 'next/router'
 const NotificationComponent = () => {
+  const router = useRouter();
+  const { userId } = router.query
+  console.log(router.query)
   const [notification, setNotification] = useState([]);
 
-  useEffect(() => {
+  useEffect( async () => {
+
+    const notificationRef = collection(firestore, "notifikasi");
+    const notificationQuery = query(notificationRef, where("userId", "==", userId))
+    const notificationSnapshot = await getDocs(notificationQuery)
+
+    const notificationData = []
+
+    notificationSnapshot.forEach((notif) => {
+        notificationData.push(notif);
+    })
+
+    notificationData.sort((a,b) => b.timeStamp-a.timeStamp)
+
+    setNotification(notificationData);
     // Dummy data for notifications
-    const dummyNotifications = [
-      {
-        message: "You have ended rental on bike 1",
-        timestamp: new Date('2024-06-08T10:00:00'),
-        userID: 123
-      },
-      {
-        message: "You have ended rental on bike 2",
-        timestamp: new Date('2024-06-08T09:30:00'),
-        userID: 124
-      },
-      {
-        message: "You have started rental on bike 1",
-        timestamp: new Date('2024-06-08T09:00:00'),
-        userID: 123
-      },
-      {
-        message: "You have started rental on bike 2",
-        timestamp: new Date('2024-06-08T08:00:00'),
-        userID: 124
-      },
-      {
-        message: "You have started rental on bike 1",
-        timestamp: new Date('2024-06-08T07:00:00'),
-        userID: 123
-      },
-      {
-        message: "You have started rental on bike 2",
-        timestamp: new Date('2024-06-08T06:00:00'),
-        userID: 124
-      }
-    ];
+    // const dummyNotifications = [
+    //   {
+    //     message: "You have ended rental on bike 1",
+    //     timestamp: new Date('2024-06-08T10:00:00'),
+    //     userID: 123
+    //   },
+    //   {
+    //     message: "You have ended rental on bike 2",
+    //     timestamp: new Date('2024-06-08T09:30:00'),
+    //     userID: 124
+    //   },
+    //   {
+    //     message: "You have started rental on bike 1",
+    //     timestamp: new Date('2024-06-08T09:00:00'),
+    //     userID: 123
+    //   },
+    //   {
+    //     message: "You have started rental on bike 2",
+    //     timestamp: new Date('2024-06-08T08:00:00'),
+    //     userID: 124
+    //   },
+    //   {
+    //     message: "You have started rental on bike 1",
+    //     timestamp: new Date('2024-06-08T07:00:00'),
+    //     userID: 123
+    //   },
+    //   {
+    //     message: "You have started rental on bike 2",
+    //     timestamp: new Date('2024-06-08T06:00:00'),
+    //     userID: 124
+    //   }
+    // ];
 
-    // Sort notifications in descending order based on timestamp
-    dummyNotifications.sort((a, b) => b.timestamp - a.timestamp);
+    // // Sort notifications in descending order based on timestamp
+    // dummyNotifications.sort((a, b) => b.timestamp - a.timestamp);
 
-    // Set the notification state
-    setNotification(dummyNotifications);
+    // // Set the notification state
+    // setNotification(dummyNotifications);
   }, []);
 
-  // Helper function to format date and time
-  const formatDate = (date) => {
-    return `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`;
-  };
-
   return (
-    <div className={styles.container}>
+    <div style={{padding: 20}}>
+      <p style={{textAlign: 'center'}}>NOTIFICATION</p>
       {notification && notification.map((notif, index) => (
-        <div key={index} className={styles.notification}>
-          <p className={styles.message}>{notif.message}</p>
-          <p className={styles.timestamp}>{formatDate(notif.timestamp)}</p>
-          <p className={styles.userID}>UserID: {notif.userID}</p>
+        <div key={index} style={{borderRadius: 20, backgroundColor: '#FFD4A8', marginTop: 20, height: 'fit-content', padding: 20}}>
+          <p style={{textAlign: 'center'}}>{notif.message}</p>
+          <p style={{textAlign: 'center', color: '#898B8C'}}>{notif.timeStamp.toString()}</p>
         </div>
       ))}
     </div>
